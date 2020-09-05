118d79a4dbf86534dd7a4e0dffa06d78ac620fdc
sobhan <sobhan.aryannamazi@gmail.com>
2020-09-05 16:24:46 +0430

    Replaced Models with Sensifai Models

--- enhancement/src/main/java/com/sensifai/enhancement/SNPE/Constant.java
+++ enhancement/src/main/java/com/sensifai/enhancement/SNPE/Constant.java
@@ -22,57 +22,9 @@ class Constant {
                     1 / 255.0, 1 / 255.0, 1 / 255.0);
 
     static final Map<String, ModelInfo> AllModels = new LinkedHashMap<String, ModelInfo>() {{
-        put("FEQE - 1", new ModelInfo("model_scale_s_2_SCE_sigmoid-20", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 2", new ModelInfo("FEQE-20", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 3", new ModelInfo("FEQE_SCE2_Without_GAP_without_normalize_layer_9_50-20-20", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 4", new ModelInfo("FEQE_SCE2_Without_GAP_without_normalize_layer_8_84-20", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 5", new ModelInfo("FEQE_SCE2_With_GAP_without_normalize_layer_8_84-20", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 6", new ModelInfo("FEQE_SCE2_7_77_feature_16_block_10-20", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 7", new ModelInfo("Original_FEQE", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 8", new ModelInfo("Original_FEQE_with_Normalize_Restore_layer", false,
-                512, 512, 512, 512, 1, FEQE_No_PreprocessInfo));
-        put("FEQE - 9", new ModelInfo("model_scale_s_4-20_2", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 10", new ModelInfo("model_scale_s_4-20", false,
-                512, 512, 512, 512, 1, FEQE_No_PreprocessInfo));
-        put("FEQE - 11", new ModelInfo("FEQE_SCE_scale_4_desubpixel_without_conv-20", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 12", new ModelInfo("FEQE_SCE_scale_8_desubpixel_without_conv-20", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 13", new ModelInfo("FEQE_SCE_scale_4__desubpixel_without_conv_5_block", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 14", new ModelInfo("FEQE_SCE_scale_4_5_Block_7-41_score_1_desubpixel", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 15", new ModelInfo("FEQE_SE_scale_4_5_Block_1_desubpixel", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 16", new ModelInfo("FEQE_SCE_scale_8_3_block_1_desubpixel", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 17", new ModelInfo("FEQE_SCE_Scale8_Score6.31_ResBlock3_Final", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 18", new ModelInfo("FEQE_SE_Scale4_Score7.57_ResBlock5_Final", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("FEQE - 19", new ModelInfo("FEQE_SE_5B_7.57_852x480", false,
+        put("Enhancement", new ModelInfo("Sensifai_Enhancement_SNPE", false,
                 852, 480, 852, 480, 1, FEQE_PreprocessInfo));
-        put("FEQE - 20", new ModelInfo("FEQE_SE_5B_7.57_shape_5_852_480_3", false,
-                852, 480, 852, 480, 5, FEQE_PreprocessInfo));
-        put("FEQE - 21", new ModelInfo("FEQE_SE_5B_7.57_shape_10_852_480_3", false,
-                852, 480, 852, 480, 10, FEQE_PreprocessInfo));
-        put("FEQE - 22", new ModelInfo("FEQE_SE_5B_7.57_shape_15_852_480_3", false,
-                852, 480, 852, 480, 15, FEQE_PreprocessInfo));
-        put("FEQE - 23", new ModelInfo("Model_S4_R5_F48_S7.57_WNR_852x480", false,
+        put("SuperResolution", new ModelInfo("Sensifai_SuperResolution_SNPE", false,
                 852, 480, 852, 480, 1, FEQE_PreprocessInfo));
-        put("SuperResolution - FEQE1", new ModelInfo("Original_SP_FEQE", false,
-                512, 512, 512, 512, 1, FEQE_PreprocessInfo));
-        put("SuperResolution - FEQE2", new ModelInfo("Original_SP_FEQE_852x480", false,
-                852, 480, 852, 480, 1, FEQE_PreprocessInfo));
-//        put("EnlightenGAN", new ModelInfo("EnlightenGAN", false,
-//                400,608,400, 608, EnlightenGAN_PreprocessInfo));
     }};
 }
--- enhancement/src/main/java/com/sensifai/enhancement/TFLite/Enhancement.java
+++ enhancement/src/main/java/com/sensifai/enhancement/TFLite/Enhancement.java
@@ -20,80 +20,11 @@ public class Enhancement extends Processor<EnhancementResult> {
     private final boolean isDynamicInput;
 
     private static final Map<String, ModelInfo<EnhancementResult>> EnhancementModels = new LinkedHashMap<String, ModelInfo<EnhancementResult>>() {{
-        put("Enhancement - FEQE", new ModelInfo<>(
-                "FEQE_SCE2_7_77_feature_16_block_10_float16_quantization_20.tflite", null,
+        put("Enhancement", new ModelInfo<>(
+                "Sensifai_Enhancement_TFLite.tflite", null,
                 ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE2", new ModelInfo<>(
-                "FEQE_SCE2_7_77_feature_16_block_10-20.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE3", new ModelInfo<>(
-                "FEQE_SCE2_without_normalize_layer_9_50_float16_quantization_Node.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE4", new ModelInfo<>(
-                "bahri_7_77_feature_16_block_10_20_Without_Normalize_and_with_Gl.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE5", new ModelInfo<>(
-                "FEQE_SCE2_7_77_feature_16_block_10-20_new.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE6", new ModelInfo<>(
-                "bahri_7_77_feature_16_block_10_20_Without_Normalize_and_with_Gl (2).tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE7", new ModelInfo<>(
-                "bahri_7_77_feature_16_block_10_20_without_Normalize_Clip_with_Global.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE8", new ModelInfo<>(
-                "FEQE_SE_Scale2_Feature16_Score7_81_Without_Normalize_Clip_With_Global.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE9", new ModelInfo<>(
-                "FEQE_SE_Scale2_Feature16_Score7_81_without_Normalize_Clip_ADD_with.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE10", new ModelInfo<>(
-                "model_s2_full_20_without_Normalize_Clip_ADD_with_Global_aki.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE11", new ModelInfo<>(
-                "model_s2_full-20_without_Normalize_Clip_Add.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE12", new ModelInfo<>(
-                "FEQE_SE_Scale2_Feature16_Score7_81_Without_Normalize_Clip_With_Global.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE13", new ModelInfo<>(
-                "model_without_scale_add.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE14", new ModelInfo<>(
-                "model_s2_full-20without_scale_with_everything.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE15", new ModelInfo<>(
-                "Final.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE16", new ModelInfo<>(
-                "FEQE_SE_NoScale_R10_S9.76_Without_N_C_with_G_tf15_Float16.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE17", new ModelInfo<>(
-                "FEQE_SE_NoScale_R10_S9_76_Without_N_C_with_G_tf2_3n_Default.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE18", new ModelInfo<>(
-                "FEQE_SE_S4_R5_S7.57_Without_N_C_with_G_tf2.3n_Default.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE19", new ModelInfo<>(
-                "FEQE_SE_S4_R5_S7.57_Without_N_C_with_G_tf2.3n_Float16.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE20", new ModelInfo<>(
-                "FEQE_SE_NoScale_R10_S9_76_Without_N_C_with_G_tf2_3n_Float16.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE21", new ModelInfo<>(
-                "FEQE_SE_S4_R5_S7.57_Without_N_C_with_G_tf2.3n_Float16_V2.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("Enhancement - FEQE22", new ModelInfo<>(
-                "FEQE_SCE_S2_F16_R20_S9.47_without_N_C_with_G_tf2.3n_New.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_FEQE(), new PostProcess_FEQE()));
-        put("SuperResolution - FEQE1", new ModelInfo<>(
-                "Original_FEQE_SuperResolution.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_SuperResolution_FEQE(), new PostProcess_SuperResolution_FEQE()));
-        put("SuperResolution - FEQE2", new ModelInfo<>(
-                "Original_FEQE_SuperResolution_without_Normalize_and_clip.tflite", null,
-                ModelInfo.ModelType.TFLite, new PreProcess_SuperResolution_FEQE(), new PostProcess_SuperResolution_FEQE()));
-        put("SuperResolution - FEQE3", new ModelInfo<>(
-                "model.ckpt_Original.tflite", null,
+        put("SuperResolution", new ModelInfo<>(
+                "Sensifai_SuperResolution_TFLite.tflite", null,
                 ModelInfo.ModelType.TFLite, new PreProcess_SuperResolution_FEQE(), new PostProcess_SuperResolution_FEQE()));
 
     }};
